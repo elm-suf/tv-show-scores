f9033637f7918d3c988a6ab0bf939466581dfe39
elm-suf <elm.sufian@gmail.com>
2024-11-23 12:17:39 +0100

    tailwind

--- src/app/pages/home.ts
+++ src/app/pages/home.ts
@@ -9,6 +9,7 @@ const CORE_IMPORTS = [CommonModule];
   imports: [CORE_IMPORTS],
   template: `
     <main>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
       <input
         type="text"
         name="search"
